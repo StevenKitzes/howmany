96709ff96399640ad4989e3bc4be6934e30f4516
Steven Kitzes <steven.kitzes@gmail.com>
2015-12-29 14:26:07 -0800

    hid some notifications behind 'debug' mode

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var DEBUG = true;
+var DEBUG = false;
 var MAX_DECIMAL_FACTOR = 4;
 var conversionValue = 29.5735;
 
@@ -195,72 +195,77 @@ function updateResult() {
 	var oz = parseFloat(textOZ.value);
 	var abv = parseFloat(textABV.value);
 	var cost = parseFloat(textCost.value);
-	
+
 	// begin status notifications
-	resultHTML.push('<ul class="list-group">');
+	debugPushString(resultHTML, '<ul class="list-group">');
 	
 	if(textML.value == '' || textOZ.value == '' || ml == 0 || oz == 0) {
 		errFound = true;
-		resultHTML.push('<li class="list-group-item list-group-item-danger">');
-		resultHTML.push('<img class="icon v-align-middle" src="img/err-mark.png"> ');
-		resultHTML.push('<span class="v-align-middle">');
-		resultHTML.push('Invalid volume');
-		resultHTML.push('</span>');
-		resultHTML.push('</li>');
+		debugPushString(resultHTML, '<li class="list-group-item list-group-item-danger">');
+		debugPushString(resultHTML, '<img class="icon v-align-middle" src="img/err-mark.png"> ');
+		debugPushString(resultHTML, '<span class="v-align-middle">');
+		debugPushString(resultHTML, 'Invalid volume');
+		debugPushString(resultHTML, '</span>');
+		debugPushString(resultHTML, '</li>');
 	}
 	else {
-		resultHTML.push('<li class="list-group-item list-group-item-success">');
-		resultHTML.push('<img class="icon v-align-middle" src="img/check-mark.png"> ');
-		resultHTML.push('<span class="v-align-middle">');
-		resultHTML.push('Registered ' + oz + ' fl.oz. (' + ml + ' ml)');
-		resultHTML.push('</span>');
-		resultHTML.push('</li>');
+		debugPushString(resultHTML, '<li class="list-group-item list-group-item-success">');
+		debugPushString(resultHTML, '<img class="icon v-align-middle" src="img/check-mark.png"> ');
+		debugPushString(resultHTML, '<span class="v-align-middle">');
+		debugPushString(resultHTML, 'Registered ' + oz + ' fl.oz. (' + ml + ' ml)');
+		debugPushString(resultHTML, '</span>');
+		debugPushString(resultHTML, '</li>');
 	}
 	
 	if(textABV.value == '' || abv == 0) {
 		errFound = true;
-		resultHTML.push('<li class="list-group-item list-group-item-danger">');
-		resultHTML.push('<img class="icon v-align-middle" src="img/err-mark.png"> ');
-		resultHTML.push('<span class="v-align-middle">');
-		resultHTML.push('Invalid ABV');
-		resultHTML.push('</span>');
-		resultHTML.push('</li>');
+		debugPushString(resultHTML, '<li class="list-group-item list-group-item-danger">');
+		debugPushString(resultHTML, '<img class="icon v-align-middle" src="img/err-mark.png"> ');
+		debugPushString(resultHTML, '<span class="v-align-middle">');
+		debugPushString(resultHTML, 'Invalid ABV');
+		debugPushString(resultHTML, '</span>');
+		debugPushString(resultHTML, '</li>');
 	}
 	else {
-		resultHTML.push('<li class="list-group-item list-group-item-success">');
-		resultHTML.push('<img class="icon v-align-middle" src="img/check-mark.png"> ');
-		resultHTML.push('<span class="v-align-middle">');
-		resultHTML.push('Registered ABV of ' + abv + '%');
-		resultHTML.push('</span>');
-		resultHTML.push('</li>');
+		debugPushString(resultHTML, '<li class="list-group-item list-group-item-success">');
+		debugPushString(resultHTML, '<img class="icon v-align-middle" src="img/check-mark.png"> ');
+		debugPushString(resultHTML, '<span class="v-align-middle">');
+		debugPushString(resultHTML, 'Registered ABV of ' + abv + '%');
+		debugPushString(resultHTML, '</span>');
+		debugPushString(resultHTML, '</li>');
 	}
 	
 	if(textCost.value == '' || cost == 0) {
-		resultHTML.push('<li class="list-group-item list-group-item-warning">');
-		resultHTML.push('<img class="icon v-align-middle" src="img/warn-mark.png"> ');
-		resultHTML.push('<span class="v-align-middle">');
-		resultHTML.push('No cost entered');
-		resultHTML.push('</span>');
-		resultHTML.push('</li>');
+		debugPushString(resultHTML, '<li class="list-group-item list-group-item-warning">');
+		debugPushString(resultHTML, '<img class="icon v-align-middle" src="img/warn-mark.png"> ');
+		debugPushString(resultHTML, '<span class="v-align-middle">');
+		debugPushString(resultHTML, 'No cost entered');
+		debugPushString(resultHTML, '</span>');
+		debugPushString(resultHTML, '</li>');
 	}
 	else {
-		resultHTML.push('<li class="list-group-item list-group-item-success">');
-		resultHTML.push('<img class="icon v-align-middle" src="img/check-mark.png"> ');
-		resultHTML.push('<span class="v-align-middle">');
-		resultHTML.push('Registered cost of $' + cost);
-		resultHTML.push('</span>');
-		resultHTML.push('</li>');
+		debugPushString(resultHTML, '<li class="list-group-item list-group-item-success">');
+		debugPushString(resultHTML, '<img class="icon v-align-middle" src="img/check-mark.png"> ');
+		debugPushString(resultHTML, '<span class="v-align-middle">');
+		debugPushString(resultHTML, 'Registered cost of $' + cost);
+		debugPushString(resultHTML, '</span>');
+		debugPushString(resultHTML, '</li>');
 	}
 	
 	// end status notifications
-	resultHTML.push('</ul>');
-	
+	debugPushString(resultHTML, '</ul>');
+
 	resultHTML.push(generateStats(errFound, genderDefined, oz, abv));
 	
 	resultHTML.push(errFound ? '' : '<br><input type="button" value="Again!" onclick="textOZ.focus(); textOZ.select(); inputDiv.scrollIntoView(true);">');
 	
     resultSpan.innerHTML = resultHTML.join('');
 	dOut('Updates so far: ' + DEBUG_updateCounter);
+}
+
+// pushes the given string onto the end of the given array, only if DEBUG is enabled
+function debugPushString(arr, str) {
+	if(DEBUG) arr.push(str);
 }
 
 function generateStats(err, genderDefined, oz, abv) {
@@ -289,7 +294,7 @@ function generateStats(err, genderDefined, oz, abv) {
 				'When can you drive:',
 				'<span class="badge">At least ' + (totalDrinks * 2 / 3).toFixed(2) + ' hours</span>',
 			'</li>',
-			'<li class="list-group-item">',
+			'<li class="list-group-item' + (textCost.value == '' || textCost.value == '0' ? ' list-group-item-warning' : '') + '">',
 				'Cost per drink:',
 				'<span class="badge">' + (textCost.value == '' || textCost.value == '0' ? 'No cost entered.' : ('$' + (parseFloat(textCost.value)/totalDrinks).toFixed(2))) + '</span>',
 			'</li>',
