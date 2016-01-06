bd07f88573f99932dbb7b17c539e3679352c3034
Steven Kitzes <steven.kitzes@gmail.com>
2016-01-06 12:59:00 -0800

    added logic and results display items for BAC and safe driving time calculations

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 var DEBUG = false;
 var MAX_DECIMAL_FACTOR = 4;
-var conversionValue = 29.5735;
-NORMALIZE_OZ = 12; // set by FDA
-NORMALIZE_ABV = 5; // set by FDA
+var VOLUME_CONVERSION = 29.5735;
+var WEIGHT_CONVERSION = 454;
+var NORMALIZE_OZ = 12; // set by FDA
+var NORMALIZE_ABV = 5; // set by FDA
+var OUNCES_PER_DRINK = 0.6; // ounces of pure alcohol per US standard drink
+var OUNCES_PER_POUND = 16; // ounces of weight per US pound
+var ALCOHOL_GRAMS_PER_ML = 0.789; // from NDAA.org
+var MALE_WIDMARK = 0.68;
+var FEMALE_WIDMARK = 0.55;
+var METABOLIC_AVERAGE = 0.015;
 
 var DEBUG_updateCounter;
 var clearButton;
@@ -70,10 +77,14 @@ function clickHandler(clickEvent) {
 	if(clickEvent.target == radioMale) {
 		radioMale.classList.add('active');
 		radioFemale.classList.remove('active');
+		updateResult();
+		return;
 	}
 	if(clickEvent.target == radioFemale) {
 		radioMale.classList.remove('active');
 		radioFemale.classList.add('active');
+		updateResult();
+		return;
 	}
 }
 
@@ -99,7 +110,7 @@ function handleKeyUp(keyEvent) {
 		}
 		else {
 			var ml = parseFloat(textML.value);
-			var oz = ml / conversionValue;
+			var oz = ml / VOLUME_CONVERSION;
 			textOZ.value = oz;
 		}
 	}
@@ -110,7 +121,7 @@ function handleKeyUp(keyEvent) {
 		}
 		else {
 			var oz = parseFloat(textOZ.value);
-			var ml = oz * conversionValue;
+			var ml = oz * VOLUME_CONVERSION;
 			textML.value = ml;
 		}
 	}
@@ -158,7 +169,7 @@ function updateML() {
 	}
 	else {
 		var oz = parseFloat(textOZ.value);
-		var ml = oz * conversionValue;
+		var ml = oz * VOLUME_CONVERSION;
 		textML.value = ml;
 	}
 }
@@ -173,6 +184,7 @@ function updateResult() {
 	var ml = parseFloat(textML.value);
 	var oz = parseFloat(textOZ.value);
 	var abv = parseFloat(textABV.value);
+	var weight = parseFloat(textWeight.value);
 	var cost = parseFloat(textCost.value);
 
 	// begin status notifications
@@ -234,7 +246,7 @@ function updateResult() {
 	// end status notifications
 	debugPushString(resultHTML, '</ul>');
 
-	resultHTML.push(generateStats(errFound, oz, abv));
+	resultHTML.push(generateStats(errFound, oz, abv, weight));
 	
 	resultSpan.innerHTML = resultHTML.join('');
 	dOut('Updates so far: ' + DEBUG_updateCounter);
@@ -247,13 +259,31 @@ function debugPushString(arr, str) {
 
 // param 'oz' is the volume given by the user
 // param 'abv' is the abv rating given by the user
-function generateStats(err, oz, abv) {
+function generateStats(err, oz, abv, weight) {
     dOut('generating results');
     if(err) return '<p class="slim"><em>Complete the form without errors to get your results!</em></p>';
 	
 	var ratioABV = abv / NORMALIZE_ABV;
 	var ratioOZ = oz / NORMALIZE_OZ;
 	var totalDrinks = ratioABV * ratioOZ;
+	
+	var maleSelected = radioMale.classList.contains('active');
+	var femaleSelected = radioFemale.classList.contains('active');
+	var haveGender = maleSelected ? !femaleSelected : femaleSelected;
+	var haveWeight = (textWeight.value != '' && parseFloat(textWeight.value) != 0);
+	
+	var maxBAC = 0;
+
+	// Widmark's Formula
+	if(haveGender && haveWeight) {
+		var genderConstant = (maleSelected ? MALE_WIDMARK : FEMALE_WIDMARK);
+		var ml = oz * VOLUME_CONVERSION;
+		var dose = ml * (abv/100) * ALCOHOL_GRAMS_PER_ML;
+		var weightInGrams = weight * WEIGHT_CONVERSION;
+		maxBAC = (dose / (weightInGrams * genderConstant)) * 100;
+	}
+	var hoursTillDrive = maxBAC / METABOLIC_AVERAGE;
+	var minutesTillDrive = hoursTillDrive * 60;
 	
 	var ozToOneDrink = oz / totalDrinks;
 	var ozToOneDrinkHTML = [];
@@ -277,26 +307,28 @@ function generateStats(err, oz, abv) {
 				totalDrinks.toFixed(2),
 				'</span>',
 			'</li>',
-			'<li class="list-group-item',
-			(totalDrinks > 2 ? ' list-group-item-danger' : (totalDrinks > 1 ? ' list-group-item-warning' : ' list-group-item-success')),
-			'">',
-				'In a 24 hour period:',
-				'<span class="badge">',
-				(totalDrinks > 2 ? 'FDA says no' : (totalDrinks > 1 ? 'Men yes, women no' : 'FDA says okay')),
+			'<li class="list-group-item">',
+				'Cancer risk:',
+				'<span class="badge' + (totalDrinks > 2 ? '-right colors-danger' : (totalDrinks > 1 ? '-right colors-warning' : '')) + '">',
+				(totalDrinks > 2 ? 'high' : (totalDrinks > 1 ? 'medium' : 'low')),
 				'</span>',
 			'</li>',
-			'<li class="list-group-item list-group-item-danger">',
-				'Wait to drive:',
-				'<span class="badge">',
-				(totalDrinks * 2 / 3).toFixed(2),
-				' hours after last sip</span>',
+			'<li class="list-group-item">',
+				'Est. Max. BAC:',
+				'<span class="badge-right ' + (maxBAC >= 0.8 ? 'colors-danger' : 'colors-warning') + '">',
+				(haveGender ? (haveWeight ? truncateZeroes(maxBAC.toFixed(4)) : 'need weight to calculate') : 'need gender to calculate'),
+				'</span>',
 			'</li>',
-			'<li class="list-group-item',
-			(textCost.value == '' || textCost.value == '0' ? ' list-group-item-warning' : ''),
-			'">',
+			'<li class="list-group-item">',
+				'Wait to drive:',
+				'<span class="badge-right ' + (maxBAC == 0 ? 'colors-warning' : 'colors-danger') + '">',
+				(maxBAC == 0 ? 'need BAC to calculate' : Math.floor(hoursTillDrive) + ' hours ' + Math.ceil(minutesTillDrive%60) + ' minutes'),
+				'</span>',
+			'</li>',
+			'<li class="list-group-item">',
 				'Cost per drink:',
-				'<span class="badge">',
-				(textCost.value == '' || textCost.value == '0' ? 'No cost entered.' : ('$' + (parseFloat(textCost.value)/totalDrinks).toFixed(2))),
+				'<span class="badge' + (textCost.value == '' || textCost.value == '0' ? '-right colors-warning' : '') + '">',
+				(textCost.value == '' || textCost.value == '0' ? 'No cost entered' : ('$' + (parseFloat(textCost.value)/totalDrinks).toFixed(2))),
 				'</span>',
 			'</li>',
 		'</ul>',
